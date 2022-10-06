f99c997ef4161816686cc626aa2611d875cc7688
Vladislav Noskov <noskov.vlad@mail.ru>
2022-10-06 20:45:02 +0300

    autotest1

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
   const status = err.statusCode || 500;
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
-  res.status(status).send({ message });
+  res.status(statusCode).send({ message });
   next();
 };
 
