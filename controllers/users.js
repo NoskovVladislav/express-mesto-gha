0f694cb15c8e7e6fe2998c442d00afd52f271b36
Vladislav Noskov <noskov.vlad@mail.ru>
2022-10-11 18:02:50 +0300

    autotest 2.1

--- controllers/users.js
+++ controllers/users.js
@@ -118,8 +118,8 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return Users.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key' , { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.send({ message: 'Авторизация успешна', token });
     })
-    .catch(next(err));
+    .catch(next);
 };
