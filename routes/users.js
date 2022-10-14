c4847f789c6f4a5a03fb8415001b8717ca2fdee2
Vladislav Noskov <noskov.vlad@mail.ru>
2022-10-14 16:05:25 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,7 @@ const {
 } = require('../controllers/users');
 
 router.get('/me', auth, getUserMe);
+router.get('/', auth, getUsers);
 router.get('/:userId', auth, parameterIdValid('userId'), getUserId);
 router.patch('/me', auth, userValid, updateUserInfo);
 router.patch('/me/avatar', userAvatarValid, updateAvatar);
